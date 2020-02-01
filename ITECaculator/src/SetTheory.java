2832acf326ccdbdd8c276be32abb2b9e62450783
Leang <ngounmengleang18@gmail.com>
2020-02-01 17:13:31 +0700

    set theory

--- ITECaculator/src/SetTheory.java
+++ ITECaculator/src/SetTheory.java
@@ -7,18 +7,18 @@ public class SetTheory {
     Scanner input = new Scanner(System.in);
     interface UnionHelper
     {
-        String unionOperation();
-        void display();
+        ArrayList<String> unionOperation();
+        void display(ArrayList <String> union);
     }
     interface IntersectionHelper
     {
-        String intersectionOperation();
-        void display();
+        ArrayList<String>  intersectionOperation();
+        void display(ArrayList <String> intersection);
     }
     interface SetDifferenceHelper
     {
-        String differenceOperation();
-        void display();
+        ArrayList<String>  differenceOperation();
+        void display(ArrayList <String> difference);
     }
 
     void createSet()
@@ -26,29 +26,115 @@ public class SetTheory {
         String setStringA , setStringB;
         StringBuilder builder = new StringBuilder();
         System.out.print("Please input set A : ");
-        setStringA =  input.next();
+        setStringA =  input.nextLine();
         System.out.print("Please input set B : ");
-        setStringB =  input.next();
+        setStringB =  input.nextLine();
+        setStringA = setStringA + " ";
+        setStringB =  setStringB + " ";
+        for (int i = 0 ; i < setStringA.length() ; i++)
+        {
+            if(setStringA.charAt(i) == ' ')
+            {
+                if(!setA.contains(builder.toString()))
+                {
+                    setA.add(builder.toString());
+                }
+                builder.setLength(0);
+
+            }
+            else
+            {
+                builder.append(setStringA.charAt(i));
+            }
+
+        }
+        for (int i = 0 ; i < setStringB.length() ; i++)
+        {
+            if(setStringB.charAt(i) == ' ')
+            {
+                if(!setB.contains(builder.toString()))
+                {
+                    setB.add(builder.toString());
+                }
+                builder.setLength(0);
+            }
+            else
+            {
+                builder.append(setStringB.charAt(i));
+            }
+
+        }
+
+        /*for (String set : setA)
+        {
+            System.out.println(set);
+        }
+        System.out.println("----------------------------------------------");
+        for (String set : setB)
+        {
+            System.out.println(set);
+        }*/
 
     }
 
-    class setUnion implements UnionHelper
+    class SetUnion implements UnionHelper
     {
-
         @Override
-        public String unionOperation() {
-            return null;
+        public ArrayList<String> unionOperation()
+        {
+            ArrayList<String> union = new ArrayList<String>();
+            union.addAll(setA);
+            for (String a : setB)
+            {
+                if (!union.contains(a))
+                {
+                    union.add(a);
+                }
+            }
+            return union;
         }
 
         @Override
-        public void display() {
-
+        public void display(ArrayList<String> union)
+        {
+            System.out.print("Union of set A and set B : ");
+            for (String a:union)
+            {
+                System.out.print(a+" ");
+            }
+            System.out.println();
         }
 
     }
     void union()
     {
-        setUnion set_union = new setUnion();
+        SetUnion set_union = new SetUnion();
         createSet();
+        set_union.display(set_union.unionOperation());
+    }
+
+    void intersection()
+    {
+        class SetIntersection implements IntersectionHelper
+        {
+
+            @Override
+            public ArrayList<String> intersectionOperation() {
+                ArrayList<String> intersection = new ArrayList<String>();
+                for (String b : setA) {
+                    for (String a : setB) {
+                        if (!intersection.contains(a)) {
+                            intersection.add(a);
+                        }
+                    }
+                }
+                return intersection;
+            }
+
+            @Override
+            public void display(ArrayList<String> intersection) {
+
+            }
+        }
     }
 }

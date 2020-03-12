96fb632c9590ec092cfcc3a1301ac17f1372be06
Leang <ngounmengleang18@gmail.com>
2020-03-12 14:44:21 +0700

    Model for GUI

---
+++ ITECaculator/src/View/MainWindow.java
@@ -0,0 +1,21 @@
+package View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextPane textPane1;
+
+    public MainWindow() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+}

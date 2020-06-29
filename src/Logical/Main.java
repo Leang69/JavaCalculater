173565a78cb52474d3cbe75d835c61baa51a2992
Leang <ngounmengleang18@gmail.com>
2020-06-29 17:21:00 +0700

    zxzx

--- src/Logical/Main.java
+++ src/Logical/Main.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {  
         MyFrame myFrame = new MyFrame();
+        //kjsdghfbdjaskfgbasdikfjgasbdfjkdasbfn
 
         
         
